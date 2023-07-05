c1b67860d2d49d43fff78823e4d217f3a3fc1edd
seokb <greensode@gmail.com>
2023-07-05 09:41:45 +0900

    커밋3 maincontroller에 내용 변경

--- src/main/java/com/team2/finalproject/controller/MainController.java
+++ src/main/java/com/team2/finalproject/controller/MainController.java
@@ -1,12 +1,12 @@
 package com.team2.finalproject.controller;
 
 import org.springframework.stereotype.Controller;
-<<<<<<< HEAD
+
 import org.springframework.web.bind.annotation.GetMapping;
-=======
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
->>>>>>> 0bfbd6604f1bb9a642f8ba6e0702631a99b5a5fc
+
 
 @Controller
 public class MainController {
@@ -17,14 +17,6 @@ public class MainController {
 		return "main";
 	}
 	
-
-	@RequestMapping(value = "/main", method = RequestMethod.GET)
-	public String main() {
-		
-		return "main";
-	}
-	
-
 
 	@RequestMapping(value = "/productdetail", method = RequestMethod.GET)
 	public String productdetail() {
