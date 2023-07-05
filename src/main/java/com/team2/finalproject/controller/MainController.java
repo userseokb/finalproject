a5355dbaa6dbea103fe6f88d492686c26d2bcd1f
seokb <greensode@gmail.com>
2023-07-05 09:54:13 +0900

    커밋4 mainController 내용 수정

--- src/main/java/com/team2/finalproject/controller/MainController.java
+++ src/main/java/com/team2/finalproject/controller/MainController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 
+public class MainController {
 	@RequestMapping(value = "/main", method = RequestMethod.GET)
 	public String main() {
 		return "main";
